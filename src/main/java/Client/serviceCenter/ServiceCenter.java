5a24804960bdd45ac1999bfc89b784afa4a14e91
hourhl <huilin8200@gmail.com>
2024-12-10 16:09:09 +0800

    delete all code and try to finish it again

--- src/main/java/Client/serviceCenter/ServiceCenter.java
+++
@@ -1,8 +0,0 @@
-package Client.serviceCenter;
-
-import java.net.InetSocketAddress;
-
-public interface ServiceCenter {
-    // 查询：根据服务名称查找地址
-    InetSocketAddress serviceDiscovery(String serviceName);
-}
