a3a7e48fadad20d7e3c126c5d39bb3976004d7f7
hourhl <huilin8200@gmail.com>
2025-08-19 17:46:07 +0800

    update

--- src/main/java/Client/retry/guavaRetry.java
+++ src/main/java/Client/retry/guavaRetry.java
@@ -18,9 +18,13 @@ public class guavaRetry {
         this.client = client;
         // 配置重试逻辑
         Retryer<RpcResponse> retryer = RetryerBuilder.<RpcResponse>newBuilder()
+                // 异常重试
                 .retryIfException()
+                // 结果重试
                 .retryIfResult(response -> Objects.equals(response.getCode(), 500))
-                .withWaitStrategy(WaitStrategies.fixedWait(2, TimeUnit.SECONDS))
+                // 等待策略：指数等待，初始为2s，最多等待32s，超过最大值后，一直保持最大值
+                .withWaitStrategy(WaitStrategies.exponentialWait(2, 32, TimeUnit.SECONDS))
+                // 最多重试次数（包含初始发送）
                 .withStopStrategy(StopStrategies.stopAfterAttempt(3))
                 .withRetryListener(new RetryListener() {
                     @Override
